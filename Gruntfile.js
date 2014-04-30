c840bfabcc82c06fdb758cf62dc072bba291e3a9
Gerome <johngerome@gmail.com>
2014-04-30 13:09:25 +0200

    starter

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+      sass: {
+        options: {
+            loadPath: ['bower_components/foundation/scss'],
+            quiet: true
+        },
+        watch: {
+          options: {
+            style: 'nested' // nested, compact, compressed, expanded.
+          },
+          files: {
+            'assets/css/app.css': 'scss/app.scss'
+          }
+        },
+
+         build: {
+          options: {
+            style: 'compressed'
+          },
+          files: {
+            'assets/css/app.css': 'scss/app.scss'
+          }
+        },
+      },
+
+      uglify: {
+        watch: {
+          options: {
+            mangle: false,
+            style: 'nested'
+          },
+          files: {
+            'assets/js/app.min.js': ['js/*.js'],
+          }
+        },
+
+        build: {
+          options: {
+            mangle: false,
+             style: 'nested'
+          },
+          files: {
+            'assets/js/scripts.min.js': ['js/include/**/*.js'],
+            'assets/js/app.min.js': ['js/*.js'],
+
+            // Exclude Script here
+
+          }
+        }
+      },
+
+      watch: {
+        options: {
+          spawn: false 
+        },
+        grunt: { files: ['Gruntfile.js'] }, 
+
+        sass: {
+          files: ['scss/*.scss', 'scss/**/**/*.scss'],
+          tasks: ['sass:watch']
+        },
+        uglify: {
+          files: ['js/*.js'],
+          tasks: ['uglify:watch']
+        }
+      }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass:build','uglify:build']);
+  grunt.registerTask('default', ['watch']);
+}
